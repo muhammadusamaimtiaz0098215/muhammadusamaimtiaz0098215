6fa4b476970c6b8c972d599c6aeee2225cd61714
Muaaz Rafi <tomuaaz@gmail.com>
2021-11-22 14:59:14 +0500

    Adding the page component for better control

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import Head from "next/head";
 import NProgress from "nprogress";
 import Router from "next/router";
 import setAxiosDefault from "../store/utils/setAxiosDefaults";
 import NoSSR from "react-no-ssr";
 import { wrapper } from "../store";
+import Page from "../components/Page";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -15,19 +16,21 @@ setAxiosDefault();
 function MyApp({ Component, pageProps }) {
   return (
     <NoSSR>
-        <Head>
-          <meta
-            name="viewport"
-            content="user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width,height=device-height"
-          />
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <link rel="shortcut icon" href="/images/favicon.png" />
-          <title>Graana Admin</title>
-        </Head>    
-      <Component {...pageProps} />
-    </NoSSR> 
-  ) 
+      <Head>
+        <meta
+          name="viewport"
+          content="user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width,height=device-height"
+        />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="shortcut icon" href="/images/favicon.png" />
+        <title>Graana Admin</title>
+      </Head>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </NoSSR>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
--- store/utils/setAxiosDefaults.js
+++ store/utils/setAxiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import router from "next/router";
 import NProgress from 'nprogress';
-// import Cookies from "js-cookie";
+import Cookies from "js-cookie";
 
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
@@ -24,21 +24,21 @@ const setAxiosDefaults = () => {
     } ,
     (error) => {
       NProgress.done();
-      // if (error.response && error.response.status === 401) {
-      //   console.log("Auth Cookie:", Cookies.get("accessToken"));
-      //   Cookies.remove("accessToken");
-      //   Cookies.remove("currentUser");
-      //   Cookies.remove("domains");
+      if (error.response && error.response.status === 401) {
+        console.log("Auth Cookie:", Cookies.get("accessToken"));
+        Cookies.remove("accessToken");
+        Cookies.remove("currentUser");
+        Cookies.remove("domains");
 
-      //   error.response.data = {
-      //     Session: "Expired!",
-      //   };
-      //   router.push("/signin?auth_faild=true");
-      // } else if (error.response && error.response.status === 409) {
-      //   router.push("/subscription?sub_required=true");
-      // } else {
-      //   return Promise.reject(error);
-      // }
+        error.response.data = {
+          Session: "Expired!",
+        };
+        router.push("/signin?auth_faild=true");
+      } else if (error.response && error.response.status === 409) {
+        router.push("/subscription?sub_required=true");
+      } else {
+        return Promise.reject(error);
+      }
     }
   );
 };
