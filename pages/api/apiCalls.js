0f0104b4cb6bd1394804818550a504dc0d8780db
Muhammad_Usama_Imtiaz <usama.imtiaz@virtual-force.com>
2021-12-02 12:28:28 +0500

    testing image calls

--- pages/api/apiCalls.js
+++ pages/api/apiCalls.js
@@ -25,10 +25,21 @@ export const CategoryAPI = () => {
   const res = axios(`${env.CategoryURL}`);
   return res;
 };
-
+// http://5ee7-103-159-79-148.ngrok.io/
+//${env.BaseURL}/api/v1/professionals/create
 export const Add_professionalsAPI = (data) => {
-  const { name, username, email, category, geocodes, city, area } = data;
-
+  const {
+    name,
+    username,
+    email,
+    category,
+    geocodes,
+    city,
+    area,
+    displaypicture,
+    portfoliopicture,
+  } = data;
+  const len = category.length;
   const res = axios.post(
     `${env.BaseURL}/api/v1/professionals/create`,
     {
@@ -39,7 +50,9 @@ export const Add_professionalsAPI = (data) => {
       location: geocodes,
       city: city,
       area: area,
+      file: displaypicture,
     },
+
     {
       headers: {
         Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjM4MjUyNzg2fQ.iGZ3umwYrYc2bvW4m3TuWkeetWiEc8W_RCPgNC-6tQk`,
@@ -47,16 +60,18 @@ export const Add_professionalsAPI = (data) => {
     }
   );
 
-  console.log(res);
   return res;
 };
 
-export const Get_Profesionals = () => {
-  const res = axios.post(`${env.BaseURL}/api/v1/professionals/filter?page=1`, {
-    professionalName: null,
-    categoryId: null,
-    rating: null,
-    completedTask: null,
-  });
+export const Get_Profesionals = (data) => {
+  const res = axios.post(
+    `${env.BaseURL}/api/v1/professionals/filter?page=${data}`,
+    {
+      professionalName: null,
+      categoryId: null,
+      rating: null,
+      completedTask: null,
+    }
+  );
   return res;
 };
